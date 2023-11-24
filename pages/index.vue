de25a1e4088cf472a8640535fec4f64613ee0219
Alex Bates <ajbates93@gmail.com>
2023-11-24 15:59:27 +0000

    chore: basic module setup

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Hello world {{ x }} {{ y }}!
+  </div>
+  <button @click="toggleDark">{{ color }}</button>
+</template>
+
+<script setup lang="ts">
+const { x, y } = useMouse()
+
+const color = useColorMode()
+
+const toggleDark = () => color.value = color.value === 'dark' ? 'light' : 'dark';
+
+</script>
